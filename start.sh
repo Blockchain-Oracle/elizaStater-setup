065157bae474f55102b5e6b7683f110b228204f0
Blockchain Oracle <abubakrjimoh16488@gmail.com>
2025-05-02 12:25:51 +0100

    updated stuff

--- start.sh
+++ start.sh
@@ -6,5 +6,19 @@ set -e
 # Navigate to eliza-starter directory
 cd eliza-starter
 
+echo "Building better-sqlite3"
+echo "Current directory: $(pwd)"
+# Build better-sqlite3
+cd node_modules/better-sqlite3 && \
+    npm run build-release
+
+# Navigate back to eliza-starter
+cd ../../
+echo "Navigated back to eliza-starter"
+echo "Current directory: $(pwd)"
+
+# Echo current time
+echo "Starting application at: $(date)"
+
 # Start the application
 pnpm run start
